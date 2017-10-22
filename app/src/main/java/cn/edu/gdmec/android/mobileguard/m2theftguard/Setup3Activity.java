c87eccd3f605def5adb2a4ba620354c657ac4f47
Slient <761471926@qq.com>
2017-10-22 21:16:17 +0800

    10.22  终极一跃！

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup3Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup3Activity.java
@@ -57,6 +57,7 @@ public class Setup3Activity extends BaseSetupActivity implements View.OnClickLis
                 break;
         }
     }
+
     @Override
     protected void onActivityResult(int requestCode,int resultCode,Intent data){
         super.onActivityResult(requestCode,resultCode,data);
