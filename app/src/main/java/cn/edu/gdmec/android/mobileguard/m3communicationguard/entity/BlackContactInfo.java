15dbb4f3447dd800aeda877008da253cdb91f99f
ChanPriLup <735085780@qq.com>
2017-11-02 15:26:49 +0800

    11.2

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/entity/BlackContactInfo.java
@@ -0,0 +1,25 @@
+package cn.edu.gdmec.android.mobileguard.m3communicationguard.entity;
+
+/**
+ * Created by student on 17/10/31.
+ */
+
+public class BlackContactInfo {
+    /**黑名单号码*/
+    public String phoneNumber;
+    /**黑名单联系人名称*/
+    public String contactName;
+    /**黑名单拦截模式 1.电话拦截 2.短信拦截 3.电话、短信都拦截*/
+    public int mode;
+    public String getModeString(int mode){
+        switch (mode){
+            case 1:
+                return "电话拦截";
+            case 2:
+                return "短信拦截";
+            case 3:
+                return "电话、短信都拦截";
+        }
+        return "";
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/receiver/InterceptCallReciever.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m3communicationguard.receiver;
+
+/**
+ * Created by student on 17/10/31.
+ */
+
+public class InterceptCallReciever {
+}
