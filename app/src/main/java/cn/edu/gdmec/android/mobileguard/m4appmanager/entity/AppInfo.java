d1b6f1bef5faea7b95efec6b6c750b4854b074a6
Slient <761471926@qq.com>
2017-11-12 21:41:22 +0800

    11.12 1!!!!

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/entity/AppInfo.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/entity/AppInfo.java
@@ -1,8 +1,31 @@
 package cn.edu.gdmec.android.mobileguard.m4appmanager.entity;
 
+
+import android.graphics.drawable.Drawable;
+
 /**
  * Created by student on 17/11/6.
  */
 
 public class AppInfo {
+    public String packageName;
+    public Drawable icon;
+    public String appName;
+    public String apkPath;
+    public long appSize;
+    public boolean isInRoom;
+    public boolean isUserApp;
+    public boolean isSelected = false;
+    public String versionName;
+    public long firstInstallTime;
+    public String signature;
+    public String requestedPermissions;
+
+    public String getAppLocation(boolean isInRoom) {
+        if (isInRoom) {
+            return "手机内存";
+        } else {
+            return "外部存储";
+        }
+    }
 }
